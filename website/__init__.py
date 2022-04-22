eb9cf9aab8ca453363eb3ab075178674f592dc9f
schmkelsosu <schmkels@oregonstate.edu>
2022-04-21 20:47:13 -0500

    starting db stuff

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def create_app():
     app = Flask(__name__)
+    app.config["SECRET_KEY"] = "helloCS-361"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
     from website.views import views
     app.register_blueprint(views, url_prefix="/")
 
