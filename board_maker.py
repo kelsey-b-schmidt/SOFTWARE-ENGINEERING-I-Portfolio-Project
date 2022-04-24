695136272819bdcd1249981ed485f01d0d8dd245
schmkelsosu <schmkels@oregonstate.edu>
2022-04-23 21:03:58 -0500

    gonna try a react frontend - may revert

--- board_maker.py
+++ board_maker.py
@@ -1,76 +1,72 @@
-from tabulate import tabulate
 import random
 from random import shuffle
 
-choices = []
 
-while choices == []:
-    table = [[0 for x in range(9)] for x in range(9)]
-    choices = choices = [1,2,3,4,5,6,7,8,9]
-    row = 0
-    for i in range(9):
-        column = 0
+def board_maker():
+    choices = []
+    while choices == []:
+        board = [[0 for x in range(9)] for x in range(9)]
+        choices = [1,2,3,4,5,6,7,8,9]
+        row = 0
         for i in range(9):
-            choices = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-            # along same column from top to current position
-            for k in range(0,row):
-                choices = [x for x in choices if x != table[k][column]]
+            column = 0
+            for j in range(9):
+                choices = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+                # along same column from top to current position
+                for k in range(0,row):
+                    choices = [x for x in choices if x != board[k][column]]
 
-            # along same row from right to current position
-            for k in range(0,column):
-                choices = [x for x in choices if x != table[row][k]]
+                # along same row from right to current position
+                for k in range(0,column):
+                    choices = [x for x in choices if x != board[row][k]]
 
-            # 2nd row of blocks
-            # 1st column
-            if column % 3 == 0 and (row+2) % 3 == 0:
-                for k in range((column+1), (column+3)):
-                    choices = [x for x in choices if x != table[row-1][k]]
-            # 2nd column
-            if (column+2) % 3 == 0 and (row+2) % 3 == 0:
-                for k in range((column-1), (column+2)):
-                    if k == column:
-                        continue
-                    choices = [x for x in choices if x != table[row-1][k]]
-            # 3rd column
-            if (column+1) % 3 == 0 and (row+2) % 3 == 0:
-                for k in range((column-2), column):
-                    choices = [x for x in choices if x != table[row-1][k]]
-
-            #3rd row of blocks
-            # 1st column
-            if column % 3 == 0 and (row+1) % 3 == 0:
-                for k in range((row-2), row):
-                    for l in range((column+1), (column+3)):
-                        choices = [x for x in choices if x != table[k][l]]
-
-            # 2nd column
-            if (column+2) % 3 == 0 and (row+1) % 3 == 0:
-                for k in range((row - 2), row):
-                    for l in range((column-1), (column+2)):
-                        if l == column:
+                # 2nd row of blocks
+                # 1st column
+                if column % 3 == 0 and (row+2) % 3 == 0:
+                    for k in range((column+1), (column+3)):
+                        choices = [x for x in choices if x != board[row-1][k]]
+                # 2nd column
+                if (column+2) % 3 == 0 and (row+2) % 3 == 0:
+                    for k in range((column-1), (column+2)):
+                        if k == column:
                             continue
-                        choices = [x for x in choices if x != table[k][l]]
+                        choices = [x for x in choices if x != board[row-1][k]]
+                # 3rd column
+                if (column+1) % 3 == 0 and (row+2) % 3 == 0:
+                    for k in range((column-2), column):
+                        choices = [x for x in choices if x != board[row-1][k]]
 
-            # 3rd column
-            if (column+1) % 3 == 0 and (row+1) % 3 == 0:
-                for k in range((row-2), row):
-                    for l in range((column-2), column):
-                        choices = [x for x in choices if x != table[k][l]]
+                #3rd row of blocks
+                # 1st column
+                if column % 3 == 0 and (row+1) % 3 == 0:
+                    for k in range((row-2), row):
+                        for l in range((column+1), (column+3)):
+                            choices = [x for x in choices if x != board[k][l]]
 
+                # 2nd column
+                if (column+2) % 3 == 0 and (row+1) % 3 == 0:
+                    for k in range((row - 2), row):
+                        for l in range((column-1), (column+2)):
+                            if l == column:
+                                continue
+                            choices = [x for x in choices if x != board[k][l]]
+
+                # 3rd column
+                if (column+1) % 3 == 0 and (row+1) % 3 == 0:
+                    for k in range((row-2), row):
+                        for l in range((column-2), column):
+                            choices = [x for x in choices if x != board[k][l]]
+
+                if choices == []:
+                    break
+                number = random.choice(choices)
+                board[row][column] = number
+                column += 1
             if choices == []:
                 break
-            number = random.choice(choices)
-            table[row][column] = number
-            column += 1
-        if choices == []:
-            break
-        row += 1
+            row += 1
 
+    return board
 
-for item in table:
-    print(item)
-
-def board_maker(table):
-    return tabulate(table, tablefmt='html')
-
-#print(board_maker(table))
+if __name__ == "__main__":
+    board_maker()
---
+++ website/main.py
@@ -0,0 +1,56 @@
+import json
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import jsonify
+from flask_pymongo import PyMongo
+import random
+from board_maker import board_maker
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "helloCS-361"
+
+mongodb_client = PyMongo(app, uri="mongodb://localhost:27017/sudoku")
+db = mongodb_client.db
+
+
+@app.route("/", methods=["GET"])
+def home_page_unchecked():
+    return render_template("main.html")
+
+
+@app.route("/new_game", methods=["GET"])
+def new_game_get():
+    return render_template("main.html")
+
+@app.route("/new_game", methods=["POST"])
+def new_game_post():
+    checkbox = request.form.get('checkbox')
+    new_game = request.form.get('new_game')
+    with open('files/button_receiver.txt', 'w') as file:
+        file.write("new_game")
+    puzzle_id = random.randint(1, 1000)
+    puzzle = board_maker()
+    db.boards.insert_one({'puzzle_id': puzzle_id, 'puzzle': puzzle})
+
+    if checkbox == "1":
+        # with open('files/button_receiver.txt', 'w') as file:
+        # file.write("toggle-on")
+        return render_template("new_game.html", check_val="0", src="static/images/checkbox.png")
+    # elif checkbox == "0":
+    # with open('files/button_receiver.txt', 'w') as file:
+    # file.write("toggle-off")
+
+    # if new_game == "1":
+    #    with open('files/button_receiver.txt', 'w') as file:
+    #        file.write("new_game")
+    # else:
+    #    with open('files/button_receiver.txt', 'w') as file:
+    #        file.write("")
+    return render_template("new_game.html", check_val="1", src="static/images/checkbox_unchecked.png")
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
